0930fc0c5855cfb7a2d7888dfebbd685abcf4e75
umaralfaruqdev <ualfaruq59@gmail.com>
2020-06-25 14:02:57 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {
+	  	fullscreen: false
+	  };
+	}
+
+	handleClick = () => {
+		if( ! this.state.fullscreen) {
+			this.setState({ fullscreen: true });
+		} else {
+			this.setState({ fullscreen: false });
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<Content dataProp={this.state.fullscreen ? 'is full' : 'default'} updateProp={this.handleClick} />
+			</div>
+		);
+	}
+}
+
+class Content extends Component {
+	render() {
+		return(
+			<div>
+				<button onClick={this.props.updateProp}>fullscreen</button>
+				<p><b>{this.props.dataProp}</b></p>
+			</div>
+		);
+	}
+}
